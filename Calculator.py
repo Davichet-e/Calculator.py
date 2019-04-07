f375130e95360a0af83a53643048fa650ac0e509
Davichet-e <48657914+Davichet-e@users.noreply.github.com>
2019-04-07 16:54:20 +0200

    Fixed some bugs

--- Calculator.py
+++ Calculator.py
@@ -18,13 +18,17 @@ resetScreen = False
 
 result = 0
 
-
 #----Screen----#
 
 numberScreen = StringVar()
 
-screen = Entry(myFrame, textvariable=numberScreen, state="readonly",
-                 readonlybackground="black", bd=20, relief=FLAT, fg='#9FEAF4')
+screen = Entry(myFrame,
+               textvariable=numberScreen,
+               state="readonly",
+               readonlybackground="black",
+               bd=20,
+               relief=FLAT,
+               fg='#9FEAF4')
 screen.grid(row=1, column=1, columnspan=10, sticky='we')
 screen.config(justify="right")
 numberScreen.set("0")
@@ -108,7 +112,7 @@ def funcSubs(num):
         else:
             if counter_funcSubs == 1:
 
-                result = num1-float(num)
+                result = num1 - float(num)
 
             else:
 
@@ -123,6 +127,7 @@ def funcSubs(num):
         operation = "-"
 
         resetScreen = True
+
 
 #----funcFact----#
 
@@ -161,11 +166,11 @@ def funcMul(num):
 
         if counter_multi == 1:
 
-            result = num1*float(num)
+            result = num1 * float(num)
 
         else:
 
-            result = float(result)*float(num)
+            result = float(result) * float(num)
 
         numberScreen.set(result)
 
@@ -177,8 +182,8 @@ def funcMul(num):
 
     resetScreen = True
 
-#----funcDivision----#
 
+#----funcDivision----#
 
 counter_divi = 0
 
@@ -201,11 +206,11 @@ def funcDiv(num):
 
         if counter_divi == 1:
 
-            result = num1/int(num)
+            result = num1 / int(num)
 
         else:
 
-            result = float(result)/float(num)
+            result = float(result) / float(num)
 
         numberScreen.set(result)
 
@@ -216,8 +221,9 @@ def funcDiv(num):
     operation = "/"
 
     resetScreen = True
-#----funcDivision----#
 
+
+#----funcDivision----#
 
 counter_remain = 0
 
@@ -256,6 +262,7 @@ def funcRemain(num):
 
     resetScreen = True
 
+
 #----funcSqrt----#
 
 
@@ -271,6 +278,7 @@ def funcSqrt(num):
 
     resetScreen = True
 
+
 #----funcSqrt----#
 
 
@@ -282,6 +290,8 @@ def funcSquare(num):
     numberScreen.set(result)
 
     resetScreen = True
+
+
 #----Trigonometric Functions----#
 
 
@@ -293,6 +303,7 @@ def funcTrigono(num, func):
     numberScreen.set(result)
 
     resetScreen = True
+
 
 #----Results----#
 
@@ -308,31 +319,32 @@ def results():
 
     if operation == "+":
 
-        numberScreen.set(float(result)+float(numberScreen.get()))
+        numberScreen.set(float(result) + float(numberScreen.get()))
 
         result = 0
 
     elif operation == "-":
 
-        numberScreen.set(float(result)-float(numberScreen.get()))
+        numberScreen.set(float(result) - float(numberScreen.get()))
         result = 0
         counter_funcSubs = 0
 
     elif operation == "*":
 
-        numberScreen.set(float(result)*float(numberScreen.get()))
+        numberScreen.set(float(result) * float(numberScreen.get()))
         result = 0
         counter_multi = 0
 
     elif operation == "/":
 
-        numberScreen.set(float(result)/float(numberScreen.get()))
+        numberScreen.set(float(result) / float(numberScreen.get()))
         result = 0
         counter_divi = 0
     elif operation == "%":
         numberScreen.set(int(result) % int(numberScreen.get()))
         result = 0
         counter_remain = 0
+
 
 #----Reset----#
 
@@ -357,112 +369,176 @@ def reset():
 
 
 #----Row0----#
-KeyReset = Button(myFrame, text='AC',
-                  command=lambda: reset(), bg='#252440', fg='white')
+KeyReset = Button(myFrame,
+                  text='AC',
+                  command=lambda: reset(),
+                  bg='#252440',
+                  fg='white')
 KeyReset.config(height=4, width=8)
 KeyReset.grid(row=2, column=0, columnspan=3, ipadx=33)
-KeySqrt = Button(myFrame, text='√n',   bg="grey",
-                 fg='white', command=lambda: funcSqrt(numberScreen.get()))
+KeySqrt = Button(myFrame,
+                 text='√n',
+                 bg="grey",
+                 fg='white',
+                 command=lambda: funcSqrt(numberScreen.get()))
 KeySqrt.config(height=4, width=8)
 KeySqrt.grid(row=2, column=3)
-KeyFact = Button(myFrame, text="n!",   bg="grey",
-                 fg='white', command=lambda: funcFact(numberScreen.get()))
+KeyFact = Button(myFrame,
+                 text="n!",
+                 bg="grey",
+                 fg='white',
+                 command=lambda: funcFact(numberScreen.get()))
 KeyFact.config(height=4, width=8)
 KeyFact.grid(row=2, column=4)
-KeyRemain = Button(myFrame, text='%',   bg="grey",
-                   fg='white', command=lambda: funcRemain(numberScreen.get()))
+KeyRemain = Button(myFrame,
+                   text='%',
+                   bg="grey",
+                   fg='white',
+                   command=lambda: funcRemain(numberScreen.get()))
 KeyRemain.config(height=4, width=8)
 KeyRemain.grid(row=2, column=5)
 
 #----Row1----#
-Key7 = Button(myFrame, text='7', bg='#81BEF7',
+Key7 = Button(myFrame,
+              text='7',
+              bg='#81BEF7',
               command=lambda: numberPressed('7'))
 Key7.config(height=4, width=8)
 Key7.grid(row=3, column=1)
-Key8 = Button(myFrame, text='8', bg='#81BEF7',
+Key8 = Button(myFrame,
+              text='8',
+              bg='#81BEF7',
               command=lambda: numberPressed('8'))
 Key8.config(height=4, width=8)
 Key8.grid(row=3, column=2)
-Key9 = Button(myFrame, text='9', bg='#81BEF7',
+Key9 = Button(myFrame,
+              text='9',
+              bg='#81BEF7',
               command=lambda: numberPressed('9'))
 Key9.config(height=4, width=8)
 Key9.grid(row=3, column=3)
-KeyDiv = Button(myFrame, text='➗',   bg="grey",
-                fg='white', command=lambda: funcDiv(numberScreen.get()))
+KeyDiv = Button(myFrame,
+                text='➗',
+                bg="grey",
+                fg='white',
+                command=lambda: funcDiv(numberScreen.get()))
 KeyDiv.config(height=4, width=8)
 KeyDiv.grid(row=3, column=4)
-KeySquare = Button(myFrame, text='x^2', bg='grey', fg='white',
+KeySquare = Button(myFrame,
+                   text='x^2',
+                   bg='grey',
+                   fg='white',
                    command=lambda: funcSquare(numberScreen.get()))
 KeySquare.config(height=4, width=8)
 KeySquare.grid(row=3, column=5)
-KeyCos = Button(myFrame, text='cos(x)', bg="grey",
-                fg='white', command=lambda: funcTrigono(numberScreen.get(), cos))
+KeyCos = Button(myFrame,
+                text='cos(x)',
+                bg="grey",
+                fg='white',
+                command=lambda: funcTrigono(numberScreen.get(), cos))
 KeyCos.config(height=4, width=8)
 KeyCos.grid(row=3, column=5)
 #----Row2----#
-Key4 = Button(myFrame, text='4', bg='#81BEF7',
+Key4 = Button(myFrame,
+              text='4',
+              bg='#81BEF7',
               command=lambda: numberPressed('4'))
 Key4.config(height=4, width=8)
 Key4.grid(row=4, column=1)
-Key5 = Button(myFrame, text='5', bg='#81BEF7',
+Key5 = Button(myFrame,
+              text='5',
+              bg='#81BEF7',
               command=lambda: numberPressed('5'))
 Key5.config(height=4, width=8)
 Key5.grid(row=4, column=2)
-Key6 = Button(myFrame, text='6', bg='#81BEF7',
+Key6 = Button(myFrame,
+              text='6',
+              bg='#81BEF7',
               command=lambda: numberPressed('6'))
 Key6.config(height=4, width=8)
 Key6.grid(row=4, column=3)
-KeyMult = Button(myFrame, text='✖', bg="grey",
-                 fg='white', command=lambda: funcMul(numberScreen.get()))
+KeyMult = Button(myFrame,
+                 text='✖',
+                 bg="grey",
+                 fg='white',
+                 command=lambda: funcMul(numberScreen.get()))
 KeyMult.config(height=4, width=8)
 KeyMult.grid(row=4, column=4)
-KeySin = Button(myFrame, text='sin(x)', bg="grey",
-                fg='white', command=lambda: funcTrigono(numberScreen.get(), sin))
+KeySin = Button(myFrame,
+                text='sin(x)',
+                bg="grey",
+                fg='white',
+                command=lambda: funcTrigono(numberScreen.get(), sin))
 KeySin.config(height=4, width=8)
 KeySin.grid(row=4, column=5)
 
 #----Row3----#
-Key1 = Button(myFrame, text='1', bg='#81BEF7',
+Key1 = Button(myFrame,
+              text='1',
+              bg='#81BEF7',
               command=lambda: numberPressed('1'))
 Key1.config(height=4, width=8)
 Key1.grid(row=5, column=1)
-Key2 = Button(myFrame, text='2', bg='#81BEF7',
+Key2 = Button(myFrame,
+              text='2',
+              bg='#81BEF7',
               command=lambda: numberPressed('2'))
 Key2.config(height=4, width=8)
 Key2.grid(row=5, column=2)
-Key3 = Button(myFrame, text='3', bg='#81BEF7',
+Key3 = Button(myFrame,
+              text='3',
+              bg='#81BEF7',
               command=lambda: numberPressed('3'))
 Key3.config(height=4, width=8)
 Key3.grid(row=5, column=3)
-KeyRest = Button(myFrame, text='➖', bg="grey",
-                 fg='white', command=lambda: funcSubs(numberScreen.get()))
+KeyRest = Button(myFrame,
+                 text='➖',
+                 bg="grey",
+                 fg='white',
+                 command=lambda: funcSubs(numberScreen.get()))
 KeyRest.config(height=4, width=8)
 KeyRest.grid(row=5, column=4)
-KeyTan = Button(myFrame, text='tan(x)', bg="grey",
-                fg='white', command=lambda: funcTrigono(numberScreen.get(), tan))
+KeyTan = Button(myFrame,
+                text='tan(x)',
+                bg="grey",
+                fg='white',
+                command=lambda: funcTrigono(numberScreen.get(), tan))
 KeyTan.config(height=4, width=8)
 KeyTan.grid(row=5, column=5)
 #----Row4----#
-Key0 = Button(myFrame, text='0', bg='#81BEF7',
+Key0 = Button(myFrame,
+              text='0',
+              bg='#81BEF7',
               command=lambda: numberPressed('0'))
 Key0.config(height=4, width=8)
 Key0.grid(row=6, column=1)
-KeyComma = Button(myFrame, text=',', bg="grey",
-                  fg='white', command=lambda: numberPressed("."))
+KeyComma = Button(myFrame,
+                  text=',',
+                  bg="grey",
+                  fg='white',
+                  command=lambda: numberPressed("."))
 KeyComma.config(height=4, width=8)
 KeyComma.grid(row=6, column=2)
-KeyEqual = Button(myFrame, text='=', bg="grey",
-                  fg='white', command=lambda: results())
+KeyEqual = Button(myFrame,
+                  text='=',
+                  bg="grey",
+                  fg='white',
+                  command=lambda: results())
 KeyEqual.config(height=4, width=8)
 KeyEqual.grid(row=6, column=3)
-KeySum = Button(myFrame, text='➕', bg="grey",
-                fg='white', command=lambda: funcSum(numberScreen.get()))
+KeySum = Button(myFrame,
+                text='➕',
+                bg="grey",
+                fg='white',
+                command=lambda: funcSum(numberScreen.get()))
 KeySum.config(height=4, width=8)
 KeySum.grid(row=6, column=4)
-KeyLog = Button(myFrame, text='ln(x)', bg="grey",
-                fg='white', command=lambda: funcTrigono(numberScreen.get(), log))
+KeyLog = Button(myFrame,
+                text='ln(x)',
+                bg="grey",
+                fg='white',
+                command=lambda: funcTrigono(numberScreen.get(), log))
 KeyLog.config(height=4, width=8)
 KeyLog.grid(row=6, column=5)
-
 
 root.mainloop()
